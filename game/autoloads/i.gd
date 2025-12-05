150b9de7024e1edbff5d36d0865817995764039d
Piotr Gawle <ptr.gawle@gmail.com>
2025-12-05 11:12:27 +0100

    pick up can

--- game/autoloads/i.gd
+++ game/autoloads/i.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_inventory.gd"
 
 # classes ----
+const PIICan := preload("res://game/inventory_items/can/inventory_item_can.gd")
 # ---- classes
 
 # nodes ----
+var Can: PIICan : get = get_Can
 # ---- nodes
 
 # functions ----
+func get_Can() -> PIICan: return get_item_instance("Can")
 # ---- functions
 
